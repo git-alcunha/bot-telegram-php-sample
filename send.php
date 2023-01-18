d25f654b55aeda274a5d320e43b0e87c700136ef
git-alcunha <122943880+git-alcunha@users.noreply.github.com>
2023-01-17 22:09:17 -0300

    Update send.php

--- send.php
+++ send.php
@@ -1,6 +1,15 @@
 <?php
 include "config.php";
 
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+if (!$update) {  exit; }
 
+$chatID = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+
+$url = URL . API . "/sendMessage?chat_id=" . $chatID . "&text=" . $message;
+    
+file_get_contents($url);
 
 ?>
