2e39dd4bd206d9aec219fbcf54736ce87f18e929
DIEGO FERNANDES DA CRUZ MORAES <122943880+git-alcunha@users.noreply.github.com>
2023-01-17 22:18:53 -0300

    Create basic-security.php

---
+++ basic-security.php
@@ -0,0 +1,45 @@
+<?php
+include "config.php";
+
+// https://core.telegram.org/bots/samples/hellobot
+
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+if (!$update) {  exit; }
+
+
+function exec_curl_request($handle) {
+  $response = curl_exec($handle);
+
+  if ($response === false) {
+    $errno = curl_errno($handle);
+    $error = curl_error($handle);
+    error_log("Curl returned error $errno: $error\n");
+    curl_close($handle);
+    return false;
+  }
+
+  $http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE));
+  curl_close($handle);
+
+  if ($http_code >= 500) {
+    // do not wat to DDOS server if something goes wrong
+    sleep(10);
+    return false;
+  } else if ($http_code != 200) {
+    $response = json_decode($response, true);
+    error_log("Request has failed with error {$response['error_code']}: {$response['description']}\n");
+    if ($http_code == 401) {
+      throw new Exception('Invalid access token provided');
+    }
+    return false;
+  } else {
+    $response = json_decode($response, true);
+    if (isset($response['description'])) {
+      error_log("Request was successful: {$response['description']}\n");
+    }
+    $response = $response['result'];
+  }
+
+  return $response;
+}
